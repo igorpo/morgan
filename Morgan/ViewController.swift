487acf21a6775848bba01073dbbd840fae666916
Igor Pogorelskiy <igorpogorelskiy@Igors-MacBook-Pro.local>
2015-03-27 15:19:02 -0400

    scrollable view added, keyboard moves with textfield now

--- Morgan/ViewController.swift
+++ Morgan/ViewController.swift
@@ -9,11 +9,14 @@
 import UIKit
 
 
-class ViewController: UIViewController, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate {
+    
+    let BOTTOM_CONSTRAINT: CGFloat = 10.0
 
     @IBOutlet var tableView: UITableView!
     @IBOutlet var messageTextField: UITextField!
-
+    @IBOutlet var sendConstraint: NSLayoutConstraint!
+    @IBOutlet var txtFieldConstraint: NSLayoutConstraint!
     var messages: [Message] = []
     var randomAnswers: [String] = ["Hi there, how can I help?", "On it, gimme a sec!", "Sorry, I don't understand",
                                     "That's what I thought", "Yup, sounds good"]
@@ -22,25 +25,35 @@ class ViewController: UIViewController, UITableViewDelegate {
             var message:Message = Message(content: messageTextField.text, isMorgan: false)
             messages.append(message)
             self.tableView.reloadData()
-            var timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: Selector("morganAnswers"), userInfo: nil, repeats: false)
+            var timer = NSTimer.scheduledTimerWithTimeInterval(0.7, target: self, selector: Selector("morganAnswers"), userInfo: nil, repeats: false)
         } else {
             
         }
+        messageTextField.text = ""
     }
     
     func morganAnswers () {
         let randomIndex = Int(arc4random_uniform(UInt32(randomAnswers.count)))
         var content = randomAnswers[randomIndex]
         var message:Message = Message(content: content, isMorgan: true)
+        messages.append(message)
         self.tableView.reloadData()
     }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.delegate = self;
+        self.tableView.delegate = self
+        messageTextField.delegate = self
         
-        // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardDidHideNotification, object: nil)
+
+        txtFieldConstraint.constant = BOTTOM_CONSTRAINT
+        sendConstraint.constant = BOTTOM_CONSTRAINT
+        var swipe: UISwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "closeKeyboard");
+        swipe.direction = UISwipeGestureRecognizerDirection.Down
+        self.view.addGestureRecognizer(swipe)
     }
 
     override func didReceiveMemoryWarning() {
@@ -71,57 +84,59 @@ class ViewController: UIViewController, UITableViewDelegate {
             cellIdentifier = "userMessageCell"
         }
         
-        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
     
-        cell.textLabel.text = messages[indexPath.row].content
+        cell.textLabel?.text = messages[indexPath.row].content
     
         return cell
     }
+    
+    // resign text field on return key press
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        txtFieldConstraint.constant = BOTTOM_CONSTRAINT
+        sendConstraint.constant = BOTTOM_CONSTRAINT
+        return true
+    }
+    
+    func closeKeyboard() {
+        self.messageTextField.resignFirstResponder()
+        txtFieldConstraint.constant = BOTTOM_CONSTRAINT
+        sendConstraint.constant = BOTTOM_CONSTRAINT
+    }
+    
+    func keyboardWillBeHidden(sender: NSNotification) {
+        if let userInfo = sender.userInfo {
+            if let keyboardHeight = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue().size.height {
+                txtFieldConstraint.constant = BOTTOM_CONSTRAINT
+                sendConstraint.constant = BOTTOM_CONSTRAINT
+                let insets: UIEdgeInsets = UIEdgeInsetsMake(self.tableView.contentInset.top, 0, keyboardHeight, 0)
+                
+                self.tableView.contentInset = insets
+                self.tableView.scrollIndicatorInsets = insets
+            }
+        }
+    }
+    
+    func keyboardWillShow(sender: NSNotification) {
+        if let userInfo = sender.userInfo {
+            if let keyboardHeight = userInfo[UIKeyboardFrameEndUserInfoKey]?.CGRectValue().size.height {
+                txtFieldConstraint.constant = keyboardHeight + BOTTOM_CONSTRAINT
+                sendConstraint.constant = keyboardHeight + BOTTOM_CONSTRAINT
+                let insets: UIEdgeInsets = UIEdgeInsetsMake(self.tableView.contentInset.top, 0, keyboardHeight, 0)
+                
+                self.tableView.contentInset = insets
+                self.tableView.scrollIndicatorInsets = insets
+                
+                self.tableView.contentOffset = CGPointMake(self.tableView.contentOffset.x, self.tableView.contentOffset.y + keyboardHeight)
+                UIView.animateWithDuration(0.3, animations: { () -> Void in
+                    self.view.layoutIfNeeded()
+                })
+            }
+        }
+    }
 
     
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return NO if you do not want the specified item to be editable.
-    return true
-    }
-    */
     
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-    if editingStyle == .Delete {
-    // Delete the row from the data source
-    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-    } else if editingStyle == .Insert {
-    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }
-    }
-    */
-    
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-    
-    }
-    */
-    
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-    // Return NO if you do not want the item to be re-orderable.
-    return true
-    }
-    */
-    
-    /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-    }
-    */
 }
 
