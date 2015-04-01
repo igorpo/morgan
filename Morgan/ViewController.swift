8c28900a8eeaba259d80e283a48a23b4c63b9bc6
Yagil <yagilbu@gmail.com>
2015-03-31 22:09:14 -0400

    fixed bubbles going nuts

--- Morgan/ViewController.swift
+++ Morgan/ViewController.swift
@@ -18,8 +18,8 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
     @IBOutlet var sendConstraint: NSLayoutConstraint!
     @IBOutlet var txtFieldConstraint: NSLayoutConstraint!
     var messages: [Message] = []
-//    var randomAnswers: [String] = ["Hmm... I'll get back to you", "On it, gimme a sec!", "Sorry, I don't understand", "That's what I thought", "Yup, sounds good", "Let me tell you a story", "Band or DJ?", "Fuck off!", "Pleased to meet you!", "You're clearly a potato", "I really like you, we should go out...sike", "I don't really know, tell me more", "I love you too!", "The wonderful world of OZ!"]
-    var randomAnswers: [String] = ["this is a rlly kf ejrf ejrkf ewjkrf ewrjkf ewjrkf erwjewrjk ewjkrg ewkrg werkg ewtg ejkt jrtk rtkj erk erjj ekj fewjr wjer gejkg ejkg ejg ergrejg kjrg jwke gkjr g rgjkwg dgbg is an idiofgft omgfg wagvs fg i fg gvf  yougbgbgr gberg! this is a rlly kf ejrf ejrkf ewjkrf ewrjkf ewjrkf erwjewrjk ewjkrg ewkrg werkg ewtg ejkt jrtk rtkj erk erjj ekj fewjr wjer gejkg ejkg ejg ergrejg kjrg jwke gkjr g rgjkwg kierahf wfds if ht ric j rjh f bubbles!"]
+    var randomAnswers: [String] = ["Hmm... I'll get back to you", "On it, gimme a sec!", "Sorry, I don't understand", "That's what I thought", "Yup, sounds good", "Let me tell you a story", "Band or DJ?", "Fuck off!", "Pleased to meet you!", "You're clearly a potato", "I really like you, we should go out...sike", "I don't really know, tell me more", "I love you too!", "The wonderful world of OZ!"]
+//    var randomAnswers: [String] = ["this is a rlly kf ejrf ejrkf ewjkrf ewrjkf ewjrkf erwjewrjk ewjkrg ewkrg werkg ewtg ejkt jrtk rtkj erk erjj ekj fewjr wjer gejkg ejkg ejg ergrejg kjrg jwke gkjr g rgjkwg dgbg is an idiofgft omgfg wagvs fg i fg gvf  yougbgbgr gberg! this is a rlly kf ejrf ejrkf ewjkrf ewrjkf ewjrkf erwjewrjk ewjkrg ewkrg werkg ewtg ejkt jrtk rtkj erk erjj ekj fewjr wjer gejkg ejkg ejg ergrejg kjrg jwke gkjr g rgjkwg kierahf wfds if ht ric j rjh f bubbles!"]
     
     /*
      * Sends a message from the user
@@ -118,7 +118,12 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
         
         var cell: UITableViewCell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath) as UITableViewCell
         
+     //   cell.backgroundColor = UIColor.grayColor()
+//        var cell: UITableViewCell = tableView.cellForRowAtIndexPath(indexPath)!
         
+        
+        // prevent appending layers on layers
+        cell.contentView.viewWithTag(500)?.removeFromSuperview()
         
         var theLabel : UILabel = cell.viewWithTag(1) as UILabel
         theLabel.lineBreakMode = .ByWordWrapping
@@ -126,11 +131,11 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
         theLabel.text = messages[indexPath.row].content
 
         theLabel.sizeToFit()
+        
         let numCharsInLabel = countElements(theLabel.text!)
         if (numCharsInLabel < 26 && cellIdentifier == "userMessageCell") {
             theLabel.textAlignment = .Right
         }
-
         
         if cellIdentifier == "morganCell" {
             let size = NSString(string: theLabel.text!).sizeWithAttributes([NSFontAttributeName: theLabel.font])
@@ -138,11 +143,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
 //            println("x coord of label: \(theLabel.frame.origin.x)")
 //            println("size of label: \(theLabel.frame.size.width)")
 //            println("--------------------------------")
+            
             let setTxtWidth = (numCharsInLabel < 26) ? size.width : size.width / ((CGFloat) (numCharsInLabel / 32))
             let rect = CGRectMake(theLabel.frame.origin.x,
                 theLabel.frame.origin.y,
                 setTxtWidth,
                 theLabel.frame.height)
+            
             let bubbleView: UIView = UIView(frame: rect)
             bubbleView.layer.borderColor = UIColor.purpleColor().CGColor
             bubbleView.backgroundColor = UIColor.purpleColor()
@@ -150,15 +157,20 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
             bubbleView.layer.borderWidth = 1
             bubbleView.layer.cornerRadius = 12
             bubbleView.bounds = CGRectInset(bubbleView.frame, -6, -6)
+            bubbleView.tag = 500
             cell.contentView.addSubview(bubbleView)
             cell.contentView.sendSubviewToBack(bubbleView)
         } else if cellIdentifier == "userMessageCell" {
+
             let size = NSString(string: theLabel.text!).sizeWithAttributes([NSFontAttributeName: theLabel.font])
+            
+            let setTxtWidth = (numCharsInLabel < 26) ? size.width : size.width / ((CGFloat) (numCharsInLabel / 32))
             let widthOfScreen = UIScreen.mainScreen().applicationFrame.width
+            
             let rect = CGRectMake(widthOfScreen - size.width - 14,
-                theLabel.frame.origin.y + size.height - 7,
-                size.width,
-                size.height)
+                theLabel.frame.origin.y,// + theLabel.frame.size.height - 7,
+                setTxtWidth,
+                theLabel.frame.height)
             
             let bubbleView: UIView = UIView(frame: rect)
             bubbleView.layer.borderColor = UIColor.blueColor().CGColor
@@ -166,18 +178,16 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
             theLabel.textColor = UIColor.whiteColor()
             bubbleView.layer.borderWidth = 1
             bubbleView.layer.cornerRadius = 12
+            bubbleView.tag = 500
             bubbleView.bounds = CGRectInset(bubbleView.frame, -6, -6)
-            cell.addSubview(bubbleView)
-            cell.sendSubviewToBack(bubbleView)
+            cell.contentView.addSubview(bubbleView)
+            cell.contentView.sendSubviewToBack(bubbleView)
             
 //            let back = UIView(frame: cell.contentView.bounds)
 //            back.backgroundColor = UIColor.whiteColor()
 //            cell.addSubview(back)
 //            cell.sendSubviewToBack(back)
         }
-
-        
-        
         return cell
     }
     
@@ -185,10 +195,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITextFieldDelegate
     * Properly reloads the messages in the conversation to move them accordingly.
     */
     func updateTableView() {
-        self.tableView.reloadData()
-        if self.tableView.contentSize.height > self.tableView.frame.size.height {
-            self.tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: messages.count - 1, inSection: 0), atScrollPosition: UITableViewScrollPosition.Middle, animated: true)
-        }
+        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+            self.tableView.reloadData()
+        })
+
+        println("updateTableView")
+//        if self.tableView.contentSize.height > self.tableView.frame.size.height {
+//            self.tableView.scrollToRowAtIndexPath(NSIndexPath(forRow: messages.count - 1, inSection: 0), atScrollPosition: UITableViewScrollPosition.Bottom, animated: true)
+//        }
     }
     
    /*
