21cdfbc70514da67011b08b420de7d3f55bbc3f7
Sarah Krueger <skrueger@seas.upenn.edu>
2015-04-02 17:22:10 -0400

    updated nlp

--- NLP/nlp.py
+++ NLP/nlp.py
@@ -1,26 +1,62 @@
 import sys
 import nltk
-from nltk import word_tokenize
+
 
 autoresponses = ["I'm down! Show me tickets.",
 				 "Not sure. Give me more info.",
 				 "Fuck you morgan. Show me something else!"]
 
+geolocation_phrases = ["nearby", "near me", "shows", "concerts", "tonight"]
 
-def parse_text(s):
-	if s == autoresponses[0]:
-		return "Sorry, can't find those tickets yet. Feast your eyes on Cher instead http://thenetworth.net/wp-content/uploads/2013/01/CHER-NET-WORTH1.jpg !"
-	if s == autoresponses[1]:
-		return "Soon we'll be able to link to some songs. For now, here's some Cher https://www.youtube.com/watch?v=BsKbwR7WXN4"
-	if s == autoresponses[2]:
-		return "Sorry, soon we'll show other options. For now, enter a new query"
-	tokens = word_tokenize(s)
-	for token in tokens:
+
+
+def parse_text(text, return_responses):
+	if text == autoresponses[0]:
+		return_responses["Custom"] = "Sorry, can't find those tickets yet. Feast your eyes on Cher instead http://thenetworth.net/wp-content/uploads/2013/01/CHER-NET-WORTH1.jpg !"
+		return return_responses
+	if text == autoresponses[1]:
+		return_responses["Custom"] = "Soon we'll be able to link to some songs. For now, here's some Cher https://www.youtube.com/watch?v=BsKbwR7WXN4"
+		return return_responses
+	if text == autoresponses[2]:
+		return_responses["Custom"] = "Sorry, soon we'll show other options. For now, enter a new query."
+		return return_responses
+
+
+	sentences = nltk.sent_tokenize(text)
+	for sent in sentences:
+         for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+             if hasattr(chunk, 'label'):
+             	if chunk.label() == 'GSP' or chunk.label() == 'GPE':
+                	return_responses["Location"] = ' '.join(c[0] for c in chunk.leaves())
+                	return return_responses
+
+
+	for phrase in geolocation_phrases:
+		if phrase in text.lower():
+			return_responses["UseGeolocation"] = True
+			return return_responses
+
+
+ 	return_responses["Custom"] = "Sorry, didn't recognize that. Enter a new query!"            	
+	return return_responses
+
 
 
 
 if __name__ == '__main__':
 	if not len(sys.argv) > 1:
 		exit("NLP App called without text input")
+
+	text = sys.argv[1]
+	print text
+
+	return_responses = {"Location": None, 
+						"Artist": None, 
+				 		"Venue": None, 
+				 		"Custom": None, 
+				 		"UseGeolocation": None}
 	
-	text = sys.argv[1:]
+	parse_text(text, return_responses)
+
+	#for p in return_responses:	print return_responses[p]
+
