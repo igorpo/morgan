b84bba38cef44460d1dd9a90a1009d6edc52aed7
Igor Pogorelskiy <igorpogorelskiy@hntvlan569.0246.wlan.wireless-resnet.upenn.edu>
2015-04-13 22:34:45 -0400

    playback works, need to fix retrieval times

--- Server.swift
+++ Server.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 var morganResponse: String = "N/A"
+var previewURL: String = "N/A"
+
 class Server: NSObject {
  
     class func postToServer(rawUserInput: String, lat: Double, lon: Double) -> Void {
-        let request = NSMutableURLRequest(URL: NSURL(string: "https://jbartolozzi.pythonanywhere.com")!)
+        
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://jbartolozzi.pythonanywhere.com/")!)
         request.HTTPMethod = "POST"
         let postString = "user_raw_data=\(rawUserInput)&user_lat=\(lat)&user_lon=\(lon)"
         request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
@@ -32,23 +35,32 @@ class Server: NSObject {
             
         
         })
-//        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) {
-//            data, response, error in
-//            
-//            if error != nil {
-//                println("error=\(error)")
-//                return
-//            }
-//            
-//            println("response = \(response)")
-//            
-//            if let responseString = NSString(data: data, encoding: NSUTF8StringEncoding) {
-//                morganResponse = responseString
-//                println("responseString = \(responseString)")
-//            }
-//            
-//        }
-//        task.resume()
     }
     
+    class func getPreviewSong(artist_name: String) -> Void {
+        var new_name = artist_name.stringByReplacingOccurrencesOfString(" ", withString: "+", options: NSStringCompareOptions.LiteralSearch, range: nil)
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://jbartolozzi.pythonanywhere.com/\(new_name)/preview")!)
+        request.HTTPMethod = "POST"
+//        let postString = "user_raw_data=\(rawUserInput)&user_lat=\(lat)&user_lon=\(lon)"
+//        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
+        let queue: NSOperationQueue = NSOperationQueue()
+        NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: {
+            (response: NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            if error != nil {
+                println("error=\(error)")
+                return
+            }
+            println("Response: \(response)")
+            if let responseString = NSString(data: data, encoding: NSUTF8StringEncoding) {
+                previewURL = responseString
+//                return responseString
+                println("Response String: " + responseString)
+                //morganAnsweredNotification
+                NSNotificationCenter.defaultCenter().postNotificationName("previewURLNotification", object: nil)
+            }
+            
+            
+        })
+    }
+
 }
