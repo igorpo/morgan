e419ad93984f509f59e2ef4ac777f03907b530bf
Igor Pogorelskiy <igorpogorelskiy@Igors-MacBook-Pro.local>
2015-04-17 00:25:08 -0400

    json working

--- Server.swift
+++ Server.swift
@@ -15,10 +15,10 @@ class Server: NSObject {
     /*
      * Hit up the server for a JSON response
      */
-    class func postToServer(rawUserInput: String, lat: Double, lon: Double) -> Void {
+    class func postToServer(rawUserInput: String, lat: Double, lon: Double, index : Int) -> Void {
         let request = NSMutableURLRequest(URL: NSURL(string: "https://jbartolozzi.pythonanywhere.com/")!)
         request.HTTPMethod = "POST"
-        let postString = "user_raw_data=\(rawUserInput)&user_lat=\(lat)&user_lon=\(lon)"
+        let postString = "user_raw_data=\(rawUserInput)&user_lat=\(lat)&user_lon=\(lon)&index=\(index)"
         request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
         let queue: NSOperationQueue = NSOperationQueue()
         NSURLConnection.sendAsynchronousRequest(request, queue: queue, completionHandler: {
