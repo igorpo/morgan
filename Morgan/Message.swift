432aa9babc3ed897f43974ea4b51292b49656154
Igor Pogorelskiy <ipogorelskiy@gmail.com>
2015-04-30 17:39:23 -0400

    New preview embedded into cells looking sexy

--- Morgan/Message.swift
+++ Morgan/Message.swift
@@ -9,13 +9,47 @@
 import UIKit
 
 class Message: NSObject {
+
+    enum MessageType {
+        case Regular, Preview, Purchase
+    }
     
     var content: String = ""
     var isMorgan: Bool
+    var artist: String = ""
+    var previewImgUrl : String = ""
+    
+    
+    
+    var type : MessageType
+    
+
+    // 0 = regular
+    // 1 = preview
+    // 2 = purchase
     
     init(content: String, isMorgan: Bool) {
         self.content = content
         self.isMorgan = isMorgan
+        self.type = MessageType.Regular
+    }
+
+//    init(content: String, isMorgan: Bool, type: MessageType) {
+//        self.content = content
+//        self.isMorgan = isMorgan
+//        self.type = type
+//    }
+
+    init(content: String, isMorgan: Bool, type: MessageType, artist: String, previewImgUrl: String) {
+        self.content = content
+        self.isMorgan = isMorgan
+        self.type = type
+        self.artist = artist
+        self.previewImgUrl = previewImgUrl
+    }
+    
+    func getType() -> MessageType {
+        return type
     }
     
     
